60d9f722f5585724a97c6b861c902a0594233013
Caleb Madrigal <caleb.madrigal@gmail.com>
2013-08-06 16:06:01 +0000

    Fixed Disqus comments, and tweaked the tags and categories urls.

--- blog/pelicanconf.py
+++ blog/pelicanconf.py
@@ -3,20 +3,36 @@
 from __future__ import unicode_literals
 import os.path
 
+
+####### High-level stuff #######
 AUTHOR = u'Caleb Madrigal'
 SITENAME = u'Caleb Madrigal'
 SITESUBTITLE = u'Welcome to my domain'
-SITEURL = u'http://labs.calebmadrigal.com'
-
+SITEURL = u'http://calebmadrigal.com'
 TIMEZONE = 'US/Central'
-
 DEFAULT_LANG = u'en'
 
+####### URL Setup #######
+
+# ARTICLES; Make article urls be in the form: "calebmadrigal.com/map-in-ios/"
 ARTICLE_URL = '{slug}/'
 ARTICLE_SAVE_AS = '{slug}/index.html'
+
+# TAGS; Make tag urls be in the form: "calebmadrigal.com/tags/python"
+TAGS_SAVE_AS = 'tags.html'
+TAG_URL = 'tags/{slug}/'
+TAG_SAVE_AS = TAG_URL + 'index.html'
+
+# CATEGORIES; Make category urls be in the form: "calebmadrigal.com/topics/python"
+CATEGORY_URL = 'topics/{slug}/'
+CATEGORY_SAVE_AS = CATEGORY_URL + 'index.html'
+
+
+# Disqus and Google analytics
 DISQUS_SITENAME = 'calebmadrigal'
 GOOGLE_ANALYTICS = 'UA-31212664-1'
 
+# Copying static files
 STATIC_OUT_DIR = ''
 STATIC_PATHS = ['images', 'code']
 
--- blog/publishconf.py
+++ blog/publishconf.py
@@ -10,7 +10,7 @@ import sys
 sys.path.append(os.curdir)
 from pelicanconf import *
 
-SITEURL = 'http://labs.calebmadrigal.com'
+SITEURL = 'http://calebmadrigal.com'
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
